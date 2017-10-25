873627e452ea3154cf13ffe17e8fe0862fdcf428
Antoine Latrille <antoine.latrille@gmail.com>
2017-10-25 12:01:28 +0200

    Set version to 0.1.3 and release of the package on pypi

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.3b2',
+    version='0.1.3',
 
     description='Check python packages from requirement.txt and report issues',
     long_description=long_description,
@@ -44,7 +44,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
