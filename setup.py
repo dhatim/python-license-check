2413b8dee9f959ada6b1c291da9259151dbde0c1
Olivier Chédru <ochedru@dhatim.com>
2018-07-22 19:52:34 +0200

    Try to make pytest work with python 2.7

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],
+    install_requires=['argparse'],
 
     python_requires='>=2.7',
     # List additional groups of dependencies here (e.g. development
@@ -88,22 +88,26 @@ setup(
     # $ pip install -e .[dev,test]
     extras_require={
         ':python_version < "3.4"': [
+            'ConfigParser',
             'enum34',
+        ],
+        ':python_version >= "3.4"': [
+            'configparser',
         ],
     },
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
-    #package_data={
+    # package_data={
     #    'sample': ['package_data.dat'],
-    #},
+    # },
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],
+    # data_files=[('my_data', ['data/data_file'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
