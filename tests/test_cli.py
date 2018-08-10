5be236f3022caa907d7ba07b40722f4a43e2fbd8
Steven Hiscocks <sdhiscocks@dstl.gov.uk>
2018-08-10 13:52:10 +0100

    Add different levels of check (#16)
    Add different levels of check
    - Standard (default): Current behaviour; at least one authorized license
    - Cautious: Must be no unauthorized licenses
    - Paranoid: All licenses found must be in authorized list

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,13 +1,15 @@
-from liccheck.command_line import parse_args, read_strategy, run
+from liccheck.command_line import parse_args, read_strategy, run, Level
 
 
 def test_parse_arguments():
     args = parse_args(['--sfile', 'my_strategy.ini'])
     assert args.strategy_ini_file == 'my_strategy.ini'
     assert args.requirement_txt_file == './requirements.txt'
-    args = parse_args(['--sfile', 'my_strategy.ini', '--rfile', 'my_requirements.txt'])
+    assert args.level is Level.STANDARD
+    args = parse_args(['--sfile', 'my_strategy.ini', '--rfile', 'my_requirements.txt', '--level', 'cautious'])
     assert args.strategy_ini_file == 'my_strategy.ini'
     assert args.requirement_txt_file == 'my_requirements.txt'
+    assert args.level is Level.CAUTIOUS
 
 
 def test_read_strategy():
