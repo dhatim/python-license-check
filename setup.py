7d0c725b5d9cd1d0edc43bbbf50ff4e3f4f03a73
Olivier Chédru <ochedru@dhatim.com>
2018-09-12 14:05:17 +0200

    Bump version to 0.3.2

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.3.1',
+    version='0.3.2',
 
     description='Check python packages from requirement.txt and report issues',
     long_description=long_description,
