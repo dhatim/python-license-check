642b8294d84e69bfcafcda59d363c1b69a091c84
Olivier Chédru <ochedru@dhatim.com>
2021-03-25 18:47:34 +0100

    Use dev as version when not set

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version=environ['RELEASE_VERSION'],
+    version=environ.get('RELEASE_VERSION', 'dev'),
 
     description='Check python packages from requirement.txt and report issues',
     long_description=long_description,
